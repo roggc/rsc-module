3dbf5f82a9db18a0a2a2188f17f242a534fb491f
roger gomez <rgomez@jumpmath.es>
2023-09-29 13:43:12 +0200

    initial

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import babel from "@rollup/plugin-babel";
+
+export default [
+  {
+    input: "src/client/components/rsc.js",
+    output: {
+      format: "esm",
+      file: "dist/client.js",
+    },
+    plugins: [babel({ babelHelpers: "bundled", exclude: "node_modules/**" })],
+  },
+  {
+    input: "src/server/index.js",
+    output: {
+      format: "esm",
+      file: "dist/server.js",
+    },
+    plugins: [babel({ babelHelpers: "bundled", exclude: "node_modules/**" })],
+  },
+];
