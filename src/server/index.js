f51e27d59e9eb990566ca5e5bf2e923daeb896bc
roger gomez <rgomez@jumpmath.es>
2023-10-03 08:47:23 +0200

    add dotenv

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import { renderToPipeableStream } from "react-dom/server";
 import { renderJSXToClientJSX, stringifyJSX } from "./utils/index.js";
